1c6cc8dfffa3a69fcac9350c09d8e907f68c6876
AnabellaParedes <66015059+AnabellaParedes@users.noreply.github.com>
2020-06-24 18:00:30 -0500

    Update funcion de conteo de meses
    Nos permitira el conteo correcto de los meses para cada opcion contable que requiramos
    
    Co-Authored-By: PizarroSofia <pizarrosofia@users.noreply.github.com>

--- Funciones_contables.py
+++ Funciones_contables.py
@@ -6,3 +6,21 @@
 #mes_f =  mes de salida
 #anio_i = anio de inicio
 #anio_f = anio de salida
+
+def conteo_meses(iniciomes,finmes,inicioanio,finalanio):
+    
+    mes_i=iniciomes
+    mes_f=finmes
+    anio_i=inicioanio
+    anio_f=finalanio
+
+    #total num de meses
+    if mes_f>=mes_i:
+        if anio_f==anio_i:
+            m=(mes_f-mes_i)+1
+            
+        elif anio_f>anio_i:
+            m=(mes_f-mes_i)+12*(anio_f-anio_i)+1
+    else:
+        m=12-mes_i+mes_f+1
+    return m
